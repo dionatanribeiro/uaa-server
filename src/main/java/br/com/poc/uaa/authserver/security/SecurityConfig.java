cf0d8fc0442373776493eb16df67f91a88bfa6f5
Dionatan Ribeiro <dionatan.ribeiro@cwi.com.br>
2017-11-30 16:52:38 -0200

    ajustar SecurityEvaluationContextExtension

--- src/main/java/br/com/poc/uaa/authserver/security/SecurityConfig.java
+++ src/main/java/br/com/poc/uaa/authserver/security/SecurityConfig.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
-
-import java.security.SecureRandom;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
